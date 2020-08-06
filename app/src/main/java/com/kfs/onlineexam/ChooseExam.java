ef52e447c31ff9481851b5653153a77fffd23f37
Abdrahman AboShanab <muna_zamalek@yahoo.com>
2020-08-06 11:33:59 +0200

    last version

---
+++ app/src/main/java/com/kfs/onlineexam/ChooseExam.java
@@ -0,0 +1,14 @@
+package com.kfs.onlineexam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChooseExam extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_exam);
+    }
+}
